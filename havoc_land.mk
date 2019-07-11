4ec86da1400eaf5259bf9b1cf3ec909f4829abab
SaksHam <Saks.Ham1714@gmail.com>
2019-07-11 11:33:40 +0000

    Add

--- havoc_land.mk
+++ havoc_land.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2018 The LineageOS Project
+# Copyright (C) 2017-2018 TheHavocOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
-# Inherit some common Lineage stuff
+# Inherit some common Havoc stuff
 $(call inherit-product, vendor/havoc/config/common.mk)
 
 # Inherit from land device
@@ -40,3 +40,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="land-user 6.0.1 MMB29M V9.6.1.0.MALMIFD release-keys"
 
 BUILD_FINGERPRINT := Xiaomi/land/land:6.0.1/MMB29M/V9.6.1.0.MALMIFD:user/release-keys
+
+# Official
+HAVOC_BUILD_TYPE := Official
+
+# Maintainer Prop
+ro.havoc.maintainer=Saksham Singh
